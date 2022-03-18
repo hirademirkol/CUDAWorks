f372599b3fc545ce087fa4146ca0a0b0a94799cd
Hira Demirkol <hirayigitdemirkol@gmail.com>
2022-03-18 19:37:59 +0100

    Fixed volumePrep and added noise

--- VolRender/volumePrep.cpp
+++ VolRender/volumePrep.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <math.h>
+#include <stdlib.h>
 
 unsigned char f(int x, int y, int z);
+void applyNoise(unsigned char *data);
+
+unsigned char data[32*32*32];
 
 int main(int argc, char **argv)
 {
@@ -10,7 +14,6 @@ int main(int argc, char **argv)
     FILE *fp = fopen((char*)"data/custom.raw", "w");
     size_t size = 32 * 32 * 32 * sizeof(VolumeType);
 
-    unsigned char data[32*32*32];
 
     for(int x = 0; x < 32; x++)
     {
@@ -23,6 +26,8 @@ int main(int argc, char **argv)
         }
     }
 
+    applyNoise(data);
+
     size_t wrote = fwrite((void*)data, 1, size, fp);
     fclose(fp);
     
@@ -31,20 +36,36 @@ int main(int argc, char **argv)
 
 unsigned char f(int x, int y, int z)
 {
-    float xf = (float)x/32.0f;
-    float yf = (float)y/32.0f;
-    float zf = (float)z/32.0f;
-    float r = 0.01f;
+    float xf = (float)x/16.0f;
+    float yf = (float)y/16.0f;
+    float zf = (float)z/16.0f;
+    float r = 1.0f;
     float r_sq = r*r;
 
     //Soft sphere with r
-    float w =(xf*xf - yf*yf - zf*zf)/ r_sq;
+    float w =(xf*xf + yf*yf + zf*zf)/ r_sq;
     // Clamp to 0-1
     w = w < 0.0f ? 0.0f : w;
     w = w > 1.0f ? 1.0f : w;
     //Inverse and square
-    //w = (1-w);
-    //w = sqrt(w);
+    w = (1-w);
+    w = sqrt(w);
 
-    return (unsigned char)255*w;
+    return (unsigned char)(255*w);
+}
+
+void applyNoise(unsigned char *data)
+{
+    float a, b;
+    for(int x = 0; x < 31; x++)
+    {
+        for(int y = 0; y < 31; y++)
+        {
+            for(int z = 0; z < 31; z++)
+            {
+                a = (float)(rand() % 127) / 256;
+                data[z*1024 + y*32 + x] = 0.5f*data[z*1024 + y*32 + x] + a*data[z*1024 + y*32 + x + 1] + (1-a)*data[z*1024 + y*32 + x + 32];
+            }
+        }
+    }
 }
